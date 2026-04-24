e163469fce1bf00dba3a06f033f8778b133d8897
RANJAN SHARMA <158837706+22054204@users.noreply.github.com>
2026-04-24 22:03:41 +0530

    Time: 2 ms (3.29%) | Memory: 43.8 MB (45.71%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.java
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -13,6 +13,31 @@ class Solution {
                 space++;
             }
         }
-        return Math.abs(L-R)+space;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<moves.length();i++){
+            char c = moves.charAt(i);
+            if(c=='L'){
+                sb.append(c);
+            }else if(c=='R'){
+                sb.append(c);
+            }else{
+                if(L>=R){
+                    sb.append('L');
+                }else{
+                    sb.append('R');
+                }
+            }
+        }
+        int left = 0;
+        int right = 0;
+        for(int i=0;i<sb.length();i++){
+            char c = sb.charAt(i);
+            if(c == 'L'){
+                left++;
+            }else{
+                right++;
+            }
+        }
+        return Math.abs(left-right);
     }
 }
